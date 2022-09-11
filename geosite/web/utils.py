03666180c30705cd424e0182924e350acf0ce6e9
Meta <petmetic@gmail.com>
2022-09-11 07:44:39 +0200

    bettering the UX

--- geosite/web/utils.py
+++ geosite/web/utils.py
@@ -15,9 +15,9 @@ def txt2coordinates(input_file):
     has_header = is_it_a_header(line)
     if has_header is True:
         lines = lines[1:]
-        log.append(f"Detected header: skipping first line")
+        log.append(f"<span class='highlight'>Detected header:</span> skipping first line")
 
-    log.append(f"Processing: {len(lines)} lines")
+    log.append(f"<span class='highlight'>Processing:</span> {len(lines)} lines")
 
     if len(line) == 3:
         final_num_of_lines, skipped_num_of_lines = pars3(lines, f)
@@ -28,12 +28,12 @@ def txt2coordinates(input_file):
     else:
         final_num_of_lines = 0
         skipped_num_of_lines = len(lines)
-        log.append(f"Expected 3 or 4 columns. Found {len(lines)}.")
+        log.append(f"<span class='highlight'>Expected 3 or 4 columns. Found {len(lines)}.</span>")
         pass
 
     f.seek(0)
 
-    log.append(f"Skipped number of lines: {skipped_num_of_lines}")
-    log.append(f"Final number of lines: {final_num_of_lines}")
+    log.append(f"<span class='highlight'>Skipped number of lines:</span> {skipped_num_of_lines}")
+    log.append(f"<span class='highlight'>Final number of lines:</span> {final_num_of_lines}")
 
     return f, log
